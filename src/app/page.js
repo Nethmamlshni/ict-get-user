0c93ea73c43957c86ad605d449de25957e2bfe04
Nethmamlshni <nethmamalshani2002@gmail.com>
2025-11-12 19:41:34 +0530

    feat: Replace BookForm with AdminBookings component in Home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import AdminBookings from "../../components/admin/AdminBookings";
 export default function Home() {
   return (
     <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <BookForm />
+      <AdminBookings />
     </div>
   );
 }
